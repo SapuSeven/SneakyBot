5af1c793b582a3d98051e79d45c2357bbd2143d0
SapuSeven <sapuseven@gmail.com>
2019-09-04 19:05:15 +0200

    Improved Poke Plugin error messages and added fixed delays between pokes

--- sneakybot-plugin-poke/src/main/kotlin/com/sapuseven/sneakybot/plugin/poke/CommandPoke.kt
+++ sneakybot-plugin-poke/src/main/kotlin/com/sapuseven/sneakybot/plugin/poke/CommandPoke.kt
@@ -4,6 +4,7 @@ import com.sapuseven.sneakybot.plugins.PluggableCommand
 import com.sapuseven.sneakybot.plugins.PluginManager
 import com.sapuseven.sneakybot.utils.Command
 import com.sapuseven.sneakybot.utils.ConsoleCommand
+import kotlin.concurrent.thread
 
 class CommandPoke : PluggableCommand {
     private lateinit var manager: PluginManager
@@ -20,54 +21,57 @@ class CommandPoke : PluggableCommand {
         }
 
     override fun execute(cmd: ConsoleCommand, invokerId: Int): Boolean {
+        // simple poke
         if (cmd.paramCount() == 2) {
             return try {
                 manager.api?.pokeClient(Integer.parseInt(cmd.getParam(1)), "") != null
             } catch (e: NumberFormatException) {
                 manager.sendMessage(
-                    "You specified an invalid number!\nTry '!help poke' for more information about the correct syntax.",
+                    "You specified an invalid client id!\nTry '!help poke' for more information about the correct syntax.",
                     invokerId
                 )
                 false
             }
+            // multi poke
         } else if (cmd.paramCount() == 3 || cmd.paramCount() == 4) {
+            val count: Int
+
             try {
-                val count = Integer.parseInt(cmd.getParam(2))
+                count = Integer.parseInt(cmd.getParam(2))
                 if (count > 50) {
                     manager.sendMessage("You can't poke a client more than 50 times at once!", invokerId)
                     return false
                 }
-                for (i in 0 until count) {
-                    if (cmd.paramCount() == 4)
-                        manager.api?.pokeClient(Integer.parseInt(cmd.getParam(1)), cmd.getParam(3))
-                    else
-                        manager.api?.pokeClient(Integer.parseInt(cmd.getParam(1)), "")
-                }
-                return true
             } catch (e: NumberFormatException) {
                 manager.sendMessage(
-                    "You specified an invalid number!\nTry '!help poke' for more information about the correct syntax.",
+                    "You specified an invalid number of pokes!\nTry '!help poke' for more information about the correct syntax.",
                     invokerId
                 )
                 return false
             }
 
-        } else if (cmd.paramCount() == 4) {
             return try {
-                for (i in 0 until Integer.parseInt(cmd.getParam(2)))
-                    manager.api?.pokeClient(Integer.parseInt(cmd.getParam(1)), cmd.getParam(3))
+                multiPoke(Integer.parseInt(cmd.getParam(1)), if (cmd.paramCount() == 4) cmd.getParam(3) else "", count)
                 true
             } catch (e: NumberFormatException) {
                 manager.sendMessage(
-                    "You specified an invalid number!\nTry '!help poke' for more information about the correct syntax.",
+                    "You specified an invalid client id!\nTry '!help poke' for more information about the correct syntax.",
                     invokerId
                 )
                 false
             }
-
         } else {
             manager.sendMessage("This isn't the right way of using this command!\nTry '!help poke'", invokerId)
             return false
+        }
+    }
+
+    private fun multiPoke(clientId: Int, message: String, count: Int = 1) {
+        thread {
+            for (i in 0 until count) {
+                manager.api?.pokeClient(clientId, message)
+                Thread.sleep(500)
+            }
         }
     }
 
