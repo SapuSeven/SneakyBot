b9e5c055fe0c9af03992eb371e6fd20bd8d8f2ca
SapuSeven <sapuseven@gmail.com>
2019-09-05 18:53:59 +0200

    Added initial unit tests

---
+++ sneakybot/src/test/kotlin/com/sapuseven/sneakybot/SneakyBotTest.kt
@@ -0,0 +1,177 @@
+package com.sapuseven.sneakybot
+
+import com.github.theholywaffle.teamspeak3.TS3Api
+import com.github.theholywaffle.teamspeak3.TS3Query
+import com.github.theholywaffle.teamspeak3.api.wrapper.Channel
+import com.github.theholywaffle.teamspeak3.api.wrapper.Client
+import com.github.theholywaffle.teamspeak3.api.wrapper.ServerGroup
+import com.github.theholywaffle.teamspeak3.api.wrapper.ServerGroupClient
+import com.sapuseven.sneakybot.exceptions.NoSuchClientException
+import com.sapuseven.sneakybot.utils.SneakyBotConfig
+import com.xenomachina.argparser.ArgParser
+import io.mockk.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class SneakyBotTest {
+    @Test
+    fun run_withExistingConsoleChannel() {
+        val args = "-s localhost -p 123".split(" ").toTypedArray()
+        val botConfig = ArgParser(args).parseInto(::SneakyBotConfig)
+
+        val mockedApi = mockk<TS3Api>(relaxed = true)
+
+        every { mockedApi.whoAmI() } returns mockk {
+            every { nickname } returns "SneakyBOT"
+            every { id } returns 1
+            every { channelId } returns 1
+        }
+        every { mockedApi.channels } returns listOf(
+            Channel(
+                mapOf(
+                    Pair("channel_name", botConfig.consoleName),
+                    Pair("cid", "1")
+                )
+            )
+        )
+
+        mockkConstructor(TS3Query::class)
+
+        every { anyConstructed<TS3Query>().connect() } returns mockk()
+        every { anyConstructed<TS3Query>().api } returns mockedApi
+
+        SneakyBot(botConfig).run()
+    }
+
+    @Test
+    fun run_withSpecifiedUsername() {
+        val args = "-s localhost -p 123 -u SneakyBOT-TEST".split(" ").toTypedArray()
+        val botConfig = ArgParser(args).parseInto(::SneakyBotConfig)
+
+        val mockedApi = mockk<TS3Api>(relaxed = true)
+
+        every { mockedApi.whoAmI() } returns mockk {
+            every { nickname } returns "SneakyBOT"
+            every { id } returns 1
+            every { channelId } returns 1
+        }
+        every { mockedApi.channels } returns listOf(
+            Channel(
+                mapOf(
+                    Pair("channel_name", botConfig.consoleName),
+                    Pair("cid", "1")
+                )
+            )
+        )
+
+        mockkConstructor(TS3Query::class)
+
+        every { anyConstructed<TS3Query>().connect() } returns mockk()
+        every { anyConstructed<TS3Query>().api } returns mockedApi
+
+        SneakyBot(botConfig).run()
+
+        verify { mockedApi.setNickname("SneakyBOT-TEST") }
+    }
+
+    @Test
+    fun run_withoutExistingConsoleChannel() {
+        val args = "-s localhost -p 123".split(" ").toTypedArray()
+        val botConfig = ArgParser(args).parseInto(::SneakyBotConfig)
+
+        val mockedApi = mockk<TS3Api>(relaxed = true)
+
+        every { mockedApi.whoAmI() } returns mockk {
+            every { nickname } returns "SneakyBOT"
+            every { id } returns 1
+            every { channelId } returns -1
+        }
+
+        mockkConstructor(TS3Query::class)
+
+        every { anyConstructed<TS3Query>().connect() } returns mockk()
+        every { anyConstructed<TS3Query>().api } returns mockedApi
+
+        SneakyBot(botConfig).run()
+
+        verify { mockedApi.createChannel(botConfig.consoleName, any()) }
+    }
+
+    @Test
+    fun run_withExistingClientsInServerGroup() {
+        val args = "-s localhost -p 123".split(" ").toTypedArray()
+        val botConfig = ArgParser(args).parseInto(::SneakyBotConfig)
+
+        val mockedApi = mockk<TS3Api>(relaxed = true)
+
+        every { mockedApi.whoAmI() } returns mockk {
+            every { nickname } returns "SneakyBOT"
+            every { id } returns 1
+            every { channelId } returns -1
+        }
+        every { mockedApi.serverGroups } returns listOf(
+            ServerGroup(
+                mapOf(
+                    Pair("name", botConfig.username),
+                    Pair("sgid", "1")
+                )
+            )
+        )
+        every { mockedApi.clients } returns listOf(
+            Client(
+                mapOf(
+                    Pair("client_nickname", "TestUser"),
+                    Pair("client_unique_identifier", "TestUserUniqueIdentifier"),
+                    Pair("clid", "2")
+                )
+            )
+        )
+        every { mockedApi.getServerGroupClients(1) } returns listOf(
+            ServerGroupClient(
+                mapOf(
+                    Pair("cldbid", "2"),
+                    Pair("client_nickname", "TestUser"),
+                    Pair("client_unique_identifier", "TestUserUniqueIdentifier")
+                )
+            )
+        )
+
+        mockkConstructor(TS3Query::class)
+
+        every { anyConstructed<TS3Query>().connect() } returns mockk()
+        every { anyConstructed<TS3Query>().api } returns mockedApi
+
+        SneakyBot(botConfig).run()
+
+        verify { mockedApi.sendPrivateMessage(2, any()) }
+    }
+
+    @Test
+    fun getClientById() {
+        val args = "-s localhost -p 123".split(" ").toTypedArray()
+        val botConfig = ArgParser(args).parseInto(::SneakyBotConfig)
+
+        val mockedBot = spyk(SneakyBot(botConfig))
+        val mockedApi = mockk<TS3Api>(relaxed = true)
+
+        val client = Client(
+            mapOf(
+                Pair("client_nickname", "TestUser"),
+                Pair("client_unique_identifier", "TestUserUniqueIdentifier"),
+                Pair("clid", "2")
+            )
+        )
+
+        every { mockedApi.clients } returns listOf(
+            client
+        )
+
+        mockedBot.query = mockk {
+            every { api } returns mockedApi
+        }
+
+        Assertions.assertEquals(client, mockedBot.getClientById(2))
+        assertThrows<NoSuchClientException> { mockedBot.getClientById(3) }
+    }
+}
---
+++ sneakybot/src/test/kotlin/com/sapuseven/sneakybot/plugins/PluginLoaderTest.kt
@@ -0,0 +1,82 @@
+package com.sapuseven.sneakybot.plugins
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.io.FileFilter
+import java.io.IOException
+
+class PluginLoaderTest {
+    @Test
+    fun loadCommands() {
+        val pluginDir: File = mockk()
+
+        every { pluginDir.listFiles(any<FileFilter>()) } returns arrayOf(File("/TestPlugin.jar"))
+
+        mockkObject(PluginLoader, recordPrivateCalls = true)
+        every { PluginLoader["extractClassesFromJAR"](any<File>(), any<ClassLoader>(), any<Class<*>>()) } returns listOf(
+            mockk<PluggableCommand> {}::class.java
+        )
+
+        val commands = PluginLoader.loadCommands(pluginDir)
+
+        Assertions.assertEquals(1, commands.size)
+    }
+
+    @Test
+    fun loadCommands_returnsEmptyOnException() {
+        val pluginDir: File = mockk()
+
+        every { pluginDir.listFiles(any<FileFilter>()) } returns arrayOf(File("/TestPlugin.jar"))
+
+        mockkObject(PluginLoader, recordPrivateCalls = true)
+        every {
+            PluginLoader["extractClassesFromJAR"](
+                any<File>(),
+                any<ClassLoader>(),
+                any<Class<*>>()
+            )
+        } throws IOException()
+
+        val commands = PluginLoader.loadCommands(pluginDir)
+        Assertions.assertEquals(0, commands.size)
+    }
+
+    @Test
+    fun loadServices() {
+        val pluginDir: File = mockk()
+
+        every { pluginDir.listFiles(any<FileFilter>()) } returns arrayOf(File("/TestPlugin.jar"))
+
+        mockkObject(PluginLoader, recordPrivateCalls = true)
+        every { PluginLoader["extractClassesFromJAR"](any<File>(), any<ClassLoader>(), any<Class<*>>()) } returns listOf(
+            mockk<PluggableService>{}::class.java
+        )
+
+        val services = PluginLoader.loadServices(pluginDir)
+
+        Assertions.assertEquals(1, services.size)
+    }
+
+    @Test
+    fun loadServices_returnsEmptyOnException() {
+        val pluginDir: File = mockk()
+
+        every { pluginDir.listFiles(any<FileFilter>()) } returns arrayOf(File("/TestPlugin.jar"))
+
+        mockkObject(PluginLoader, recordPrivateCalls = true)
+        every {
+            PluginLoader["extractClassesFromJAR"](
+                any<File>(),
+                any<ClassLoader>(),
+                any<Class<*>>()
+            )
+        } throws IOException()
+
+        val services = PluginLoader.loadServices(pluginDir)
+        Assertions.assertEquals(0, services.size)
+    }
+}
