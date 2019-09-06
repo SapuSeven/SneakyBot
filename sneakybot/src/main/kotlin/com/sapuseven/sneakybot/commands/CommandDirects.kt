aac9b953404959a0dd1a81da26fc78d2a5a455fc
SapuSeven <sapuseven@gmail.com>
2019-09-06 18:43:37 +0200

    Simplified and improved code

--- sneakybot/src/main/kotlin/com/sapuseven/sneakybot/commands/CommandDirects.kt
+++ sneakybot/src/main/kotlin/com/sapuseven/sneakybot/commands/CommandDirects.kt
@@ -10,68 +10,79 @@ internal class CommandDirects(private val bot: SneakyBot) : BuiltinCommand() {
     init {
         command.commandName = "directs"
         command.addParameter("<[\"add\"|\"del\"] [client_id]>")
-        command.help = "Lists all clients that can use the direct chat, or add/remove clients to the direct client list."
+        command.help =
+            "Lists all clients that can use the direct chat, or add/remove clients to the direct client list."
     }
 
-    // TODO: Reduce the references to the bot and try to handle most logic directly within this class
     override fun execute(cmd: ConsoleCommand, invokerId: Int): Boolean {
-        return when {
-            cmd.paramCount() == 3 -> when (cmd.getParam(1)) {
-                "add" -> if (!bot.directClients.contains(Integer.parseInt(cmd.getParam(2)))) {
-                    bot.directClients.add(Integer.parseInt(cmd.getParam(2)))
-                    bot.query.api.sendPrivateMessage(
-                        Integer.parseInt(cmd.getParam(2)),
-                        "You are now using the direct chat to communicate with me."
-                    )
-                    bot.query.api.addClientToServerGroup(
-                        bot.serverGroupId,
-                        bot.query.api.getClientInfo(Integer.parseInt(cmd.getParam(2))).databaseId
-                    )
-                    val clientListMsg =
-                        StringBuilder("User added to direct client list.\n\nClients that can contact me:")
-                    for (client in bot.directClients)
-                        clientListMsg.append("\n - ").append(bot.query.api.getClientInfo(client).nickname)
-                    bot.manager.sendMessage(clientListMsg.toString(), invokerId)
-                    true
-                } else {
-                    bot.manager.sendMessage("This user is already in the direct clients list.", invokerId)
-                    false
-                }
-                "del" -> if (bot.directClients.contains(Integer.parseInt(cmd.getParam(2)))) {
-                    bot.directClients.remove(Integer.parseInt(cmd.getParam(2)))
-                    bot.query.api.sendPrivateMessage(
-                        Integer.parseInt(cmd.getParam(2)),
-                        "Direct chat is now closed. You can send me commands via the SneakyBOT Console channel."
-                    )
-                    bot.query.api.removeClientFromServerGroup(
-                        bot.serverGroupId,
-                        bot.query.api.getClientInfo(Integer.parseInt(cmd.getParam(2))).databaseId
-                    )
-                    bot.manager.sendMessage("User removed from direct client list.", invokerId)
-                    true
-                } else {
-                    bot.manager.sendMessage("This user is not in the direct clients list!", invokerId)
-                    false
-                }
-                else -> {
-                    bot.manager.sendMessage("Unknown option: " + cmd.getParam(1), invokerId)
-                    false
+        return when (cmd.paramCount()) {
+            1 -> listDirectUsers(invokerId)
+            3 -> {
+                val targetUserId = cmd.getParam(2).toIntOrNull() ?: return sendHelpMessage(invokerId)
+
+                when (cmd.getParam(1)) {
+                    "add" -> addDirectUser(targetUserId, invokerId)
+                    "del" -> removeDirectUser(targetUserId, invokerId)
+                    else -> {
+                        bot.manager.sendMessage("Unknown option: " + cmd.getParam(1), invokerId)
+                        false
+                    }
                 }
             }
-            cmd.paramCount() == 1 -> {
-                val clientListMsg = StringBuilder("\nClients that can contact me:")
-                for (client in bot.directClients)
-                    clientListMsg.append("\n - ").append(bot.query.api.getClientInfo(client).nickname)
-                bot.manager.sendMessage(clientListMsg.toString(), invokerId)
-                true
-            }
-            else -> {
-                bot.manager.sendMessage(
-                    "This isn't the right way of using this command!\nTry '!help directs'",
-                    invokerId
-                )
-                false
-            }
+            else -> sendHelpMessage(invokerId)
+        }
+    }
+
+    private fun sendHelpMessage(invokerId: Int): Boolean {
+        bot.manager.sendMessage(
+            "This isn't the right way of using this command!\nTry '!help directs'",
+            invokerId
+        )
+        return false
+    }
+
+    private fun listDirectUsers(invokerId: Int): Boolean {
+        val clientListMsg = StringBuilder("\nClients that can contact me:")
+        for (client in bot.directClients)
+            clientListMsg.append("\n - ").append(bot.query.api.getClientInfo(client).nickname)
+        bot.manager.sendMessage(clientListMsg.toString(), invokerId)
+        return true
+    }
+
+    private fun removeDirectUser(targetUserId: Int, invokerId: Int): Boolean {
+        return if (bot.directClients.contains(targetUserId)) {
+            bot.directClients.remove(targetUserId)
+            bot.query.api.sendPrivateMessage(
+                targetUserId,
+                "Direct chat is now closed. You can send me commands via the SneakyBOT Console channel."
+            )
+            bot.query.api.removeClientFromServerGroup(
+                bot.serverGroupId,
+                bot.query.api.getClientInfo(targetUserId).databaseId
+            )
+            bot.manager.sendMessage("User removed from direct client list.", invokerId)
+            true
+        } else {
+            bot.manager.sendMessage("This user is not in the direct clients list!", invokerId)
+            false
+        }
+    }
+
+    private fun addDirectUser(targetUserId: Int, invokerId: Int): Boolean {
+        return if (!bot.directClients.contains(targetUserId)) {
+            bot.directClients.add(targetUserId)
+            bot.query.api.sendPrivateMessage(targetUserId, "You are now using the direct chat to communicate with me.")
+            bot.query.api.addClientToServerGroup(
+                bot.serverGroupId,
+                bot.query.api.getClientInfo(targetUserId).databaseId
+            )
+            val clientListMsg = StringBuilder("User added to direct client list.\n\nClients that can contact me:")
+            for (client in bot.directClients) clientListMsg.append("\n - ").append(bot.query.api.getClientInfo(client).nickname)
+            bot.manager.sendMessage(clientListMsg.toString(), invokerId)
+            true
+        } else {
+            bot.manager.sendMessage("This user is already in the direct clients list.", invokerId)
+            false
         }
     }
 }
