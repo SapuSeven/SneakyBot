a040538cd84fac79d1f61382d6d493725aa01f3e
SapuSeven <sapuseven@gmail.com>
2019-09-06 21:43:48 +0200

    Minor code cleanup

--- sneakybot/src/main/kotlin/com/sapuseven/sneakybot/commands/CommandMode.kt
+++ sneakybot/src/main/kotlin/com/sapuseven/sneakybot/commands/CommandMode.kt
@@ -17,20 +17,8 @@ internal class CommandMode(private val bot: SneakyBot) : BuiltinCommand() {
     override fun execute(cmd: ConsoleCommand, invokerId: Int): Boolean {
         return if (cmd.paramCount() == 2) {
             when (cmd.getParam(1)) {
-                "direct", "d" -> return if (bot.mode != SneakyBot.MODE_DIRECT) {
-                    bot.setupDirectMode()
-                    false
-                } else {
-                    bot.manager.sendMessage("Mode is already set to DIRECT!", invokerId)
-                    false
-                }
-                "channel", "console", "c" -> return if (bot.mode != SneakyBot.MODE_CHANNEL) {
-                    bot.setupChannelMode()
-                    true
-                } else {
-                    bot.manager.sendMessage("Mode is already set to CHANNEL!", invokerId)
-                    false
-                }
+                "direct", "d" -> switchToDirect(invokerId)
+                "channel", "console", "c" -> switchToChannel(invokerId)
                 else -> {
                     bot.manager.sendMessage("Unknown mode: " + cmd.getParam(1), invokerId)
                     false
@@ -38,6 +26,26 @@ internal class CommandMode(private val bot: SneakyBot) : BuiltinCommand() {
             }
         } else {
             bot.manager.sendMessage("This isn't the right way of using this command!\nTry '!help ${command.commandName}'", invokerId)
+            false
+        }
+    }
+
+    private fun switchToChannel(invokerId: Int): Boolean {
+        return if (bot.mode != SneakyBot.MODE_CHANNEL) {
+            bot.setupChannelMode()
+            true
+        } else {
+            bot.manager.sendMessage("Mode is already set to CHANNEL!", invokerId)
+            false
+        }
+    }
+
+    private fun switchToDirect(invokerId: Int): Boolean {
+        return if (bot.mode != SneakyBot.MODE_DIRECT) {
+            bot.setupDirectMode()
+            false
+        } else {
+            bot.manager.sendMessage("Mode is already set to DIRECT!", invokerId)
             false
         }
     }
