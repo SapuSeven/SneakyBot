ab9ee7d42b6dc03ac588954a56ee4d9c3f6f8bdc
SapuSeven <sapuseven@gmail.com>
2019-09-06 20:27:57 +0200

    Code refactoring

--- sneakybot/src/main/kotlin/com/sapuseven/sneakybot/commands/CommandHelp.kt
+++ sneakybot/src/main/kotlin/com/sapuseven/sneakybot/commands/CommandHelp.kt
@@ -9,54 +9,36 @@ internal class CommandHelp(private val bot: SneakyBot) : BuiltinCommand() {
 
     init {
         command.commandName = "help"
-        command.help = "" // TODO: command help
     }
 
     override fun execute(cmd: ConsoleCommand, invokerId: Int): Boolean {
-        return when {
-            cmd.paramCount() == 1 -> {
+        return when (cmd.paramCount()) {
+            1 -> {
                 bot.manager.sendMessage(
-                    "\n" +
-                            "Available Commands:\n" +
-                            listHelp() +
-                            "\n" +
-                            "[***] ... REQUIRED\n" +
-                            "<***> ... OPTIONAL", invokerId
-                )
-                true
-            }
-            cmd.paramCount() == 2 -> {
-                val pluggableCommand = bot.getCommandByName(cmd.getParam(1))
-                if (pluggableCommand == null) {
-                    bot.manager.sendMessage("This commands help page doesn't exist in my database!", invokerId)
-                } else {
-                    val builder = StringBuilder("Usage:\n!")
-                    builder
-                        .append(pluggableCommand.command.commandName)
-                        .append(" : ")
-
-                    for (param in pluggableCommand.command.parameters) {
-                        builder
-                            .append(formatParam(param))
-                            .append(" ")
-                    }
-
-                    builder
-                        .append(pluggableCommand.command.help)
-                        .append("\n")
-
-                    bot.manager.sendMessage(builder.toString(), invokerId)
-                }
-                true
-            }
-            else -> {
-                bot.manager.sendMessage(
-                    "This isn't the right way of using this command!\nUsage: '!help <COMMAND>'",
+                    "\nAvailable Commands:${listHelp()}\n\n[***] ... REQUIRED\n<***> ... OPTIONAL",
                     invokerId
                 )
-                false
+                true
             }
+            2 -> {
+                bot.getCommandByName(cmd.getParam(1))?.let {
+                    bot.manager.sendMessage("Usage:\n${listSingleHelp(it.command)}", invokerId)
+                    true
+                } ?: run {
+                    bot.manager.sendMessage("This commands help page doesn't exist in my database!", invokerId)
+                    false
+                }
+            }
+            else -> sendHelpMessage(invokerId)
         }
+    }
+
+    private fun sendHelpMessage(invokerId: Int): Boolean {
+        bot.manager.sendMessage(
+            "This isn't the right way of using this command!\nTry '!help <COMMAND>'",
+            invokerId
+        )
+        return false
     }
 
     private fun formatParam(param: Command.Parameter): String {
@@ -68,22 +50,22 @@ internal class CommandHelp(private val bot: SneakyBot) : BuiltinCommand() {
     }
 
     private fun listHelp(): String {
-        val builder = StringBuilder()
+        var message = ""
         for (command in bot.builtinCommands + bot.commands) {
             if (command.command.commandName == this.command.commandName) continue
-            builder
-                .append("| \n| !")
-                .append(command.command.commandName)
-                .append(" ")
-            for (param in command.command.parameters) {
-                builder
-                    .append(formatParam(param))
-                    .append(" ")
-            }
-            builder.append(": ")
-
-            builder.append(command.command.help.replace("\n", "\n|     ")).append("\n")
+            message += "\n| \n| ${listSingleHelp(command.command).replace("\n", "\n|     ")}"
         }
-        return builder.toString()
+        return message
+    }
+
+    private fun listSingleHelp(command: Command): String {
+        var message = "!${command.commandName} "
+
+        for (param in command.parameters)
+            message += "${formatParam(param)} "
+
+        message += ": ${command.help}"
+
+        return message
     }
 }
