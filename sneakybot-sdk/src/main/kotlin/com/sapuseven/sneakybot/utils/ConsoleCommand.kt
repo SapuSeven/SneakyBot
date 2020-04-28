4e0c799f89cf7cadc0351e633a022e1e1f3148fc
SapuSeven <sapuseven@gmail.com>
2020-04-28 15:01:16 +0200

    Add additional documentation

--- sneakybot-sdk/src/main/kotlin/com/sapuseven/sneakybot/utils/ConsoleCommand.kt
+++ sneakybot-sdk/src/main/kotlin/com/sapuseven/sneakybot/utils/ConsoleCommand.kt
@@ -10,7 +10,20 @@ package com.sapuseven.sneakybot.utils
 class ConsoleCommand(command: String) {
     val commandName: String = ""
 
+    /**
+     * Returns the value of a parameter supplied to an issued command.
+     * [index] is `1`-indexed, as the first parameter (index `0`) is always the command name.
+     *
+     * @param index The target parameter index.
+     * @return The supplied value of the parameter.
+     */
     fun getParam(index: Int): String = ""
 
+    /**
+     * Returns the total parameter count for an issued command.
+     * The command itself counts as a parameter, so this is always greater than one.
+     *
+     * @return The total parameter count.
+     */
     fun paramCount(): Int = 0
 }
